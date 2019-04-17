73eeab8d9ebe59a7c8410a1df22a71fac68029e4
Mhea Bustria <bustr003@cougars.csusm.edu>
2019-04-16 18:33:00 -0700

    removed duplicate files

--- CalendarApplication/app/src/main/java/com/example/calendarapplication/Event.java
+++ CalendarApplication/app/src/main/java/com/example/calendarapplication/Event.java
@@ -1,17 +1,17 @@
-//pasted from Drive
+//based on Task.java
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-@Entity(tableName = "tasks")
-public class Task {
+@Entity(tableName = "events")
+public class Event {
    @PrimaryKey
    private int _id;
 
    @NonNull
-   @ColumnInfo(name = "task")
-   private String task;
+   @ColumnInfo(name = "event")
+   private String event;
 
    @NonNull
    @ColumnInfo(name = "date")
@@ -20,9 +20,9 @@ public class Task {
    public int get_id(){return _id;}
    public void set_id(int the_id){ _id = the_id;}
 
-   public String get_task(){return task;}
-   public void set_task(String the_task){task = the_task;}
-   
+   public String get_event(){return event;}
+   public void set_event(String the_event){event = the_event;}
+
    public String get_date(){return date;}
    public void set_date(String the_date){date = the_date;}
 }
--- CalendarApplication/app/src/main/java/com/example/calendarapplication/Task.java
+++ CalendarApplication/app/src/main/java/com/example/calendarapplication/Task.java
@@ -8,21 +8,17 @@ import android.support.annotation.NonNull;
 public class Task {
    @PrimaryKey
    private int _id;
-
    @NonNull
    @ColumnInfo(name = "task")
    private String task;
-
    @NonNull
    @ColumnInfo(name = "date")
    private String date;
 
    public int get_id(){return _id;}
    public void set_id(int the_id){ _id = the_id;}
-
    public String get_task(){return task;}
    public void set_task(String the_task){task = the_task;}
-   
    public String get_date(){return date;}
    public void set_date(String the_date){date = the_date;}
 }
