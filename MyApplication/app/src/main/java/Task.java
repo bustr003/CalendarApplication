eb4dbe74738ebb5dfbc0514cd128b989a4d2699e
Mhea Bustria <bustr003@cougars.csusm.edu>
2019-04-16 18:24:58 -0700

    in MyApplication: added version numbers to build.gradle in gradle folder. added dependencies to build.gradle in app folder.

--- MyApplication/app/src/main/java/Task.java
+++ MyApplication/app/src/main/java/Task.java
@@ -1,28 +1,24 @@
-//based on Task.java
+//pasted from Drive
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
-@Entity(tableName = "events")
-public class Event {
+@Entity(tableName = "tasks")
+public class Task {
    @PrimaryKey
    private int _id;
-
    @NonNull
-   @ColumnInfo(name = "event")
-   private String event;
-
+   @ColumnInfo(name = "task")
+   private String task;
    @NonNull
    @ColumnInfo(name = "date")
    private String date;
 
    public int get_id(){return _id;}
    public void set_id(int the_id){ _id = the_id;}
-
-   public String get_event(){return event;}
-   public void set_event(String the_event){event = the_event;}
-
+   public String get_task(){return task;}
+   public void set_task(String the_task){task = the_task;}
    public String get_date(){return date;}
    public void set_date(String the_date){date = the_date;}
 }
